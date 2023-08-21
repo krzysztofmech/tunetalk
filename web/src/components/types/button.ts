bea46f6322c246b2dab7ccef7edbe79f18ae92c3
Krzysztof Mech <krzysztof.mech@endora.pl>
2023-08-21 23:31:14 +0200

    add button variant type

--- web/src/components/types/button.ts
+++ web/src/components/types/button.ts
@@ -1,1 +1,9 @@
-export type variant = "text" | "contained" | "outlined";
+export type variant =
+  | "filled"
+  | "outlined"
+  | "ghost"
+  | "icon-filled"
+  | "icon-outlined"
+  | "icon-ghost"
+  | "icon-white-ghost"
+  | "link";
