0b4b71e7f9d17732e95556c9178111c4e6a38f65
Krzysztof Mech <krzysztof.mech@endora.pl>
2023-08-21 23:38:31 +0200

    delete components based on mui

--- web/src/components/types/menu.ts
+++
@@ -1,9 +0,0 @@
-import { SvgIconTypeMap } from "@mui/material";
-import { OverridableComponent } from "@mui/material/OverridableComponent";
-
-export interface IOption {
-  text: string;
-  icon?: OverridableComponent<SvgIconTypeMap<{}, "svg">> & {
-    muiName: string;
-  };
-}
