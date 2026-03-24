9cee0a5d33ccc212e3cdc9d1b0218c46c466a839
Grzegorz Gołębiowski <grzegorz.golebiowski127@gmail.com>
2026-03-24 16:02:04 +0100

    Simplify first migration script

--- apps/backend/internal/db/migrations/00001_rooms.sql
+++ apps/backend/internal/db/migrations/00001_rooms.sql
@@ -1,13 +1,8 @@
 -- +goose Up
--- +goose StatementBegin
-
 CREATE TABLE IF NOT EXISTS rooms (
     id CHAR(36) PRIMARY KEY DEFAULT (UUID()),
     name VARCHAR(255) NOT NULL
 );
--- +goose StatementEnd
 
 -- +goose Down
--- +goose StatementBegin
 DROP TABLE IF EXISTS rooms;
--- +goose StatementEnd
